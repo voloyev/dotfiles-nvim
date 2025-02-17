eae6232298da097ba75b0234e4232e36ab9390d2
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2025-02-17 21:56:00 +0200

    Add flesh

--- lua/voloyev/plugins/init.lua
+++ lua/voloyev/plugins/init.lua
@@ -98,6 +98,16 @@ return {
     end
   },
   {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "<leader>s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+    },
+  },
+  {
     'nvim-pack/nvim-spectre',
     config = function()
       require("spectre").setup({})
