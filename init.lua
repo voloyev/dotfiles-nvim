3954f55e3515fa312702486c7b9c3138018cb5d2
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2026-01-02 22:33:49 +0200

    Add 99

--- init.lua
+++ init.lua
@@ -137,6 +137,7 @@ vim.pack.add({
   { src = "https://github.com/lewis6991/gitsigns.nvim" },
   { src = "https://github.com/jiaoshijie/undotree" },
   { src = "https://github.com/folke/zen-mode.nvim" },
+  { src = "https://github.com/ThePrimeagen/99" },
 })
 
 vim.cmd([[colorscheme gruvbox]])
@@ -323,6 +324,34 @@ require("lualine").setup({
   },
 })
 
+local undotree = require('undotree')
+
+undotree.setup({
+  float_diff = true,      -- using float window previews diff, set this `true` will disable layout option
+  layout = "left_bottom", -- "left_bottom", "left_left_bottom"
+  position = "left",      -- "right", "bottom"
+  ignore_filetype = {
+    'undotree',
+    'undotreeDiff',
+    'qf',
+  },
+  window = {
+    winblend = 90,
+    border = "rounded", -- The string values are the same as those described in 'winborder'.
+  },
+  keymaps = {
+    j = "move_next",
+    k = "move_prev",
+    gj = "move2parent",
+    J = "move_change_next",
+    K = "move_change_prev",
+    ['<cr>'] = "action_enter",
+    p = "enter_diffbuf",
+    q = "quit",
+  },
+})
+
+vim.keymap.set('n', '<leader>u', undotree.toggle, { noremap = true, silent = true })
 
 require('gitsigns').setup {
   on_attach = function(bufnr)
@@ -703,3 +732,50 @@ vim.filetype.add({
 })
 
 vim.lsp.enable("clojure_lsp")
+
+local _99 = require("99")
+
+-- For logging that is to a file if you wish to trace through requests
+-- for reporting bugs, i would not rely on this, but instead the provided
+-- logging mechanisms within 99.  This is for more debugging purposes
+local cwd = vim.uv.cwd()
+local basename = vim.fs.basename(cwd)
+_99.setup({
+  logger = {
+    level = _99.DEBUG,
+    path = "/tmp/" .. basename .. ".99.debug",
+    print_on_error = true,
+  },
+
+  --- WARNING: if you change cwd then this is likely broken
+  --- ill likely fix this in a later change
+  ---
+  --- md_files is a list of files to look for and auto add based on the location
+  --- of the originating request.  That means if you are at /foo/bar/baz.lua
+  --- the system will automagically look for:
+  --- /foo/bar/AGENT.md
+  --- /foo/AGENT.md
+  --- assuming that /foo is project root (based on cwd)
+  md_files = {
+    "AGENT.md",
+  },
+})
+
+vim.keymap.set("n", "<leader>9f", function()
+  _99.fill_in_function()
+end)
+-- take extra note that i have visual selection only in v mode
+-- technically whatever your last visual selection is, will be used
+-- so i have this set to visual mode so i dont screw up and use an
+-- old visual selection
+--
+-- likely ill add a mode check and assert on required visual mode
+-- so just prepare for it now
+vim.keymap.set("v", "<leader>9v", function()
+  _99.visual()
+end)
+
+--- if you have a request you dont want to make any changes, just cancel it
+vim.keymap.set("v", "<leader>9s", function()
+  _99.stop_all_requests()
+end)
